3e518fc067d2430c23fa4d3b6c1f694cfa75aeb5
Josh Newman <Josh@Shaynas-MacBook-Pro.local>
2021-01-08 23:01:14 -0800

    initial creation of html&js files

---
+++ script.js
@@ -0,0 +1,7 @@
+function updateSlopeInput(val) {
+    document.getElementById('slopeInput').value=val; 
+  }
+
+function updateInterceptInput(val) {
+    document.getElementById('interceptInput').value=val; 
+  }
